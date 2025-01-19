fb42cb5a6c48be3c1fba6202355c002a5351039f
Abdullah Omar <mdabdullahomar97@gmail.com>
2025-01-19 14:34:57 +0600

    feat: loader component

---
+++ src/components/loader.tsx
@@ -0,0 +1,7 @@
+export const Loader = () => {
+	return (
+		<div className='fixed inset-0 flex items-center justify-center '>
+			<div className='w-8 h-8 border-4 border-blue-500 border-t-transparent rounded-full animate-spin'></div>
+		</div>
+	);
+};
