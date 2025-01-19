3c939b754d8269355af39c43f7927489ffbe0070
Abdullah Omar <mdabdullahomar97@gmail.com>
2025-01-19 17:30:58 +0600

    style: styled button

--- src/components/action-cell.tsx
+++ src/components/action-cell.tsx
@@ -5,7 +5,10 @@ export const ActionCell = ({ userId }: { userId: number }) => {
 	const navigate = useNavigate();
 
 	return (
-		<Button onClick={() => navigate(`/users/${userId}`)} className='px-3 py-2'>
+		<Button
+			onClick={() => navigate(`/users/${userId}`)}
+			className='px-3 py-2 bg-green-500 hover:bg-green-600'
+		>
 			View Details
 		</Button>
 	);
